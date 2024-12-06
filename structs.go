a46d5b05b57ebf3a1f7b53cca052588718eb69b5
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-12-06 08:31:41 +0800

    update

--- structs.go
+++ structs.go
@@ -27,7 +27,6 @@ type TreeNode[T any] struct {
 // 主結構 ChunkPipe
 type ChunkPipe[T any] struct {
 	root      *TreeNode[T] // 原有的樹
-	bptree    *BPTree[T]   // B+ Tree 索引
 	skiplist  *SkipList[T] // Skip List 索引
 	pool      *MemoryPool  // 記憶體池
 	head      *Chunk[T]    // 頭節點
@@ -41,7 +40,6 @@ type ChunkPipe[T any] struct {
 // 工廠函數：創建 ChunkPipe
 func NewChunkPipe[T any]() *ChunkPipe[T] {
 	return &ChunkPipe[T]{
-		bptree:   NewBPTree[T](),
 		skiplist: NewSkipList[T](),
 		pool:     newMemoryPool(),
 	}
